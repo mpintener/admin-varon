a1e3f4b0318a0469b855eefe3af08fc432516e02
Martina Pintener <mpintener@gmail.com>
2017-09-13 22:14:06 -0300

    Logout

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
 	selector: 'admin',
@@ -12,12 +13,20 @@ export class AdminComponent implements OnInit{
 	public token;
 
 	constructor(
-		private _userService: UserService
+		private _userService: UserService,
+		private _router: Router,
 	){}
 
 	ngOnInit(){
 		this.user = this._userService.getUser();
 		this.token = this._userService.getToken();	
+	}
+
+	logout(){
+		localStorage.clear();
+		this.user = null;
+		this.token = null;
+		this._router.navigate(['/login']);
 	}
 
 	// comprueba los datos del local storage y ante un csambio, los actualiza
